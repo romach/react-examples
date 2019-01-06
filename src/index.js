95175ba87e918d0a6d5d7c5171df423ca0a7e115
Roman Cherepanov <romach007@gmail.com>
2019-01-07 00:37:11 +0200

    Create React component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
+// import classes from modules
 import React from "react";
-console.log("Hello!");
+import ReactDom from "react-dom";
+
+// React component
+function TodoItem() {
+  // wrap expression in parentheses to avoid the pitfalls of automatic semicolon insertion
+  return (
+    // jsx syntax
+    // React element
+    <span>Drink Coffee</span>
+  );
+}
+
+function TodoList() {
+  return (
+    <ul>
+      <li><TodoItem /></li>
+      <li><TodoItem /></li>
+      <li><TodoItem /></li>
+    </ul>
+  );
+}
+
+// React element, parent node
+ReactDom.render(<TodoList />, document.getElementById('root'));
